299b4768ec88431cf3b13e0920303adda177b448
Supakorn Thongbor <63010921@kmitl.ac.th>
2024-02-28 22:49:03 +0700

    update how to play

--- Assets/Script/Menu/MainMenuUI.cs
+++ Assets/Script/Menu/MainMenuUI.cs
@@ -1,10 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
 using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
+    public GameObject howToPlayPage;
+    public GameObject noTouchBg;
+    public Canvas page1;
+    public Canvas page2;
+    public Canvas page3;
+    public Canvas page4;
+    public Canvas page5;
+    public Canvas page6;
+
+    public Button Btn1;
+    public Button Btn2;
+    public Button Btn3;
+    public Button Btn4;
+    public Button Btn5;
+    public Button Btn6;
+
+    public Button closeHTPBtn;
+
     public void OnStartGamePressed()
     {
         SceneManager.LoadScene("Lobby");
@@ -14,5 +35,151 @@ public class MainMenuUI : MonoBehaviour
     {
         SceneManager.LoadScene("Login");
         //Application.Quit();
+    }
+    void Update()
+    { 
+        if(page1.enabled == true)
+        {
+            Btn1.enabled = false;
+
+            Btn2.enabled = true;
+            Btn3.enabled = true;
+            Btn4.enabled = true;
+            Btn5.enabled = true;
+            Btn6.enabled = true;
+        }
+        if (page2.enabled == true)
+        {
+            Btn2.enabled = false;
+
+            Btn1.enabled = true;
+            Btn3.enabled = true;
+            Btn4.enabled = true;
+            Btn5.enabled = true;
+            Btn6.enabled = true;
+        }
+        if (page3.enabled == true)
+        {
+            Btn3.enabled = false;
+
+            Btn1.enabled = true;
+            Btn2.enabled = true;
+            Btn4.enabled = true;
+            Btn5.enabled = true;
+            Btn6.enabled = true;
+        }
+        if (page4.enabled == true)
+        {
+            Btn4.enabled = false;
+
+            Btn1.enabled = true;
+            Btn2.enabled = true;
+            Btn3.enabled = true;
+            Btn5.enabled = true;
+            Btn6.enabled = true;
+        }
+        if (page5.enabled == true)
+        {
+            Btn5.enabled = false;
+
+            Btn1.enabled = true;
+            Btn2.enabled = true;
+            Btn3.enabled = true;
+            Btn4.enabled = true;
+            Btn6.enabled = true;
+        }
+        if (page6.enabled == true)
+        {
+            Btn6.enabled = false;
+
+            Btn1.enabled = true;
+            Btn2.enabled = true;
+            Btn3.enabled = true;
+            Btn4.enabled = true;
+            Btn5.enabled = true;
+        }
+    }
+
+    private void Start()
+    {
+        page1.enabled = true;
+        page2.enabled = false;
+        page3.enabled = false;
+        page4.enabled = false;
+        page5.enabled = false;
+        page6.enabled = false;
+
+    }
+
+    public void HowToPlay()
+    {
+        noTouchBg.SetActive(true);
+        howToPlayPage.SetActive(true);
+    }
+
+    public void CloseHowToPlay()
+    {
+        noTouchBg.SetActive(false);
+        howToPlayPage.SetActive(false);
+    }
+
+    public void OpenPage1()
+    {
+        page1.enabled = true;
+        page2.enabled = false;
+        page3.enabled = false;
+        page4.enabled = false;
+        page5.enabled = false;
+        page6.enabled = false;
+    }
+
+    public void OpenPage2()
+    {
+        page2.enabled = true;
+        page1.enabled = false;
+        page3.enabled = false;
+        page4.enabled = false;
+        page5.enabled = false;
+        page6.enabled = false;
+    }
+
+    public void OpenPage3()
+    {
+        page3.enabled = true;
+        page1.enabled = false;
+        page2.enabled = false;
+        page4.enabled = false;
+        page5.enabled = false;
+        page6.enabled = false;
+    }
+
+    public void OpenPage4()
+    {
+        page4.enabled = true;
+        page1.enabled = false;
+        page2.enabled = false;
+        page3.enabled = false;
+        page5.enabled = false;
+        page6.enabled = false;
+    }
+
+    public void OpenPage5()
+    {
+        page5.enabled = true;
+        page1.enabled = false;
+        page2.enabled = false;
+        page3.enabled = false;
+        page4.enabled = false;
+        page6.enabled = false;
+    }
+
+    public void OpenPage6()
+    {
+        page6.enabled = true;
+        page1.enabled = false;
+        page2.enabled = false;
+        page3.enabled = false;
+        page4.enabled = false;
+        page5.enabled = false;
     }
 }
--- Assets/Script/UI/UIInvestSell.cs
+++ Assets/Script/UI/UIInvestSell.cs
@@ -74,7 +74,21 @@ public class UIInvestSell : MonoBehaviourPunCallbacks
     }
 
     public void OkSellClick()
-    {
+    {/*
+        GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].money += +Int32.Parse(sumCalculate.text);
+        photonView.RPC("UpdateKeepForSellInvest", RpcTarget.All, PhotonNetwork.LocalPlayer.ActorNumber - 1, Int32.Parse(inputNum.text));
+        photonView.RPC("UpdateSellInvestMoney", RpcTarget.All, GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].money, PhotonNetwork.LocalPlayer.ActorNumber - 1, inputNum);
+        UIController.instance.investSellCanvas.SetActive(false);
+        UIController.instance.BlurBg.SetActive(false);
+        UIController.instance.drawButton.SetActive(false);
+        //UIController.instance.cardShow.enabled = false;
+        UIController.instance.payButton.SetActive(false);
+        UIController.instance.BigPayButton.SetActive(false);
+        UIController.instance.SellButton.SetActive(false);
+        UIController.instance.cancelButton.SetActive(false);
+        UIController.instance.SmallPayButton.SetActive(false);
+        UIController.instance.passButton.SetActive(false);
+        */
         GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].money = GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].money + Int32.Parse(sumCalculate.text);
         photonView.RPC("UpdateMoney", RpcTarget.All, GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].money, PhotonNetwork.LocalPlayer.ActorNumber - 1);
         UIController.instance.investSellCanvas.SetActive(false);
@@ -129,7 +143,7 @@ public class UIInvestSell : MonoBehaviourPunCallbacks
 
         }
     
-        
+
     }
 
     public void CancelClick()
@@ -170,7 +184,23 @@ public class UIInvestSell : MonoBehaviourPunCallbacks
         GameManager.instace.playerList[x].Keep.Add(myNote2);
 
     }
+    [PunRPC]
+    void UpdateSellInvestMoney(int money, int x, int input)
+    {
+        GameManager.instace.playerList[x].money = money;
+        UIController.instance.MyMoneyText.text = GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].money.ToString();
+        //note collect
+        GameManager.Note myNote = new GameManager.Note();
+        myNote.CardName = "+ " + "Sell " + SmallDealDeckController.instance.usedCards[SmallDealDeckController.instance.cardcount - 1].cardName + " " + input.ToString();
+        myNote.price = Int32.Parse(sumCalculate.text);
+        GameManager.instace.playerList[x].Keep.Add(myNote);
 
+        GameManager.Note myNote2 = new GameManager.Note();
+        myNote2.CardName = "= ";
+        myNote2.price = GameManager.instace.playerList[x].money;
+        GameManager.instace.playerList[x].Keep.Add(myNote2);
+
+    }
 
     [PunRPC]
     void UpdateEachKeepForInvest(int countShare)
@@ -179,7 +209,7 @@ public class UIInvestSell : MonoBehaviourPunCallbacks
         if (SmallDealDeckController.instance.usedCards[SmallDealDeckController.instance.cardcount - 1].ON2U == true)
         {
 
-            if (GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].ON2UList[0].countShare > 0)
+            if (GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].hasON2U == true)
             {
                 
                 GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].ON2UList[0].countShare = countShare;
@@ -188,7 +218,7 @@ public class UIInvestSell : MonoBehaviourPunCallbacks
                 {
                     GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].ON2UList.RemoveAt(0);
                     GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].hasON2U = false;
-                    GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].ON2UList[0].pricePerShare = 0;
+                    //GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].ON2UList[0].pricePerShare = 0;
                     
                 }
             }
@@ -196,7 +226,7 @@ public class UIInvestSell : MonoBehaviourPunCallbacks
         }
         if (SmallDealDeckController.instance.usedCards[SmallDealDeckController.instance.cardcount - 1].MYT4U == true)
         {
-            if (GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].MYT4UList[0].countShare > 0)
+            if (GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].hasMYT4U == true)
             {
 
                 GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].MYT4UList[0].countShare = countShare;
@@ -205,7 +235,7 @@ public class UIInvestSell : MonoBehaviourPunCallbacks
                 {
                     GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].MYT4UList.RemoveAt(0);
                     GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].hasMYT4U = false;
-                    GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].MYT4UList[0].pricePerShare = 0;
+                    //GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].MYT4UList[0].pricePerShare = 0;
                 }
             }
 
@@ -213,7 +243,7 @@ public class UIInvestSell : MonoBehaviourPunCallbacks
         if (SmallDealDeckController.instance.usedCards[SmallDealDeckController.instance.cardcount - 1].GRO4US == true)
         {
 
-            if (GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].GRO4USList[0].countShare > 0)
+            if (GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].hasGRO4US == true)
             {
 
                 GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].GRO4USList[0].countShare = countShare;
@@ -222,7 +252,7 @@ public class UIInvestSell : MonoBehaviourPunCallbacks
                 {
                     GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].GRO4USList.RemoveAt(0);
                     GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].hasGRO4US = false;
-                    GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].GRO4USList[0].pricePerShare = 0;
+                    //GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].GRO4USList[0].pricePerShare = 0;
                 }
             }
 
@@ -230,7 +260,7 @@ public class UIInvestSell : MonoBehaviourPunCallbacks
         if (SmallDealDeckController.instance.usedCards[SmallDealDeckController.instance.cardcount - 1].OK4U == true)
         {
 
-            if (GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].OK4UList[0].countShare > 0)
+            if (GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].hasOK4U == true)
             {
 
                 GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].OK4UList[0].countShare = countShare;
@@ -239,9 +269,90 @@ public class UIInvestSell : MonoBehaviourPunCallbacks
                 {
                     GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].OK4UList.RemoveAt(0);
                     GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].hasOK4U = false;
-                    GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].OK4UList[0].pricePerShare = 0;
+                    //GameManager.instace.playerList[PhotonNetwork.LocalPlayer.ActorNumber - 1].OK4UList[0].pricePerShare = 0;
                 }
             }
+
+        }
+    }
+
+    [PunRPC]
+    void UpdateKeepForSellInvest(int x, int input)
+    {
+
+        if (SmallDealDeckController.instance.usedCards[SmallDealDeckController.instance.cardcount - 1].ON2U == true)
+        {
+
+            if (GameManager.instace.playerList[x].hasON2U == true)
+            {
+                GameManager.instace.playerList[x].ON2UList[0].pricePerShare = ((input * SmallDealDeckController.instance.usedCards[SmallDealDeckController.instance.cardcount - 1].value)
+                    + (GameManager.instace.playerList[x].ON2UList[0].countShare * GameManager.instace.playerList[x].ON2UList[0].pricePerShare))
+                    / (input + GameManager.instace.playerList[x].ON2UList[0].countShare);
+                GameManager.instace.playerList[x].ON2UList[0].countShare -= input;
+                if (GameManager.instace.playerList[x].ON2UList[0].countShare == 0)
+                {
+                    GameManager.instace.playerList[x].ON2UList.RemoveAt(0);
+                    GameManager.instace.playerList[x].hasON2U = false;
+                    //GameManager.instace.playerList[x].ON2UList[0].pricePerShare = 0;
+
+                }
+            }
+
+        }
+        if (SmallDealDeckController.instance.usedCards[SmallDealDeckController.instance.cardcount - 1].MYT4U == true)
+        {
+
+            if (GameManager.instace.playerList[x].hasMYT4U == true)
+            {
+                GameManager.instace.playerList[x].MYT4UList[0].pricePerShare = ((input * SmallDealDeckController.instance.usedCards[SmallDealDeckController.instance.cardcount - 1].value)
+                    + (GameManager.instace.playerList[x].MYT4UList[0].countShare * GameManager.instace.playerList[x].MYT4UList[0].pricePerShare))
+                    / (input + GameManager.instace.playerList[x].MYT4UList[0].countShare);
+                GameManager.instace.playerList[x].MYT4UList[0].countShare -= input;
+                if (GameManager.instace.playerList[x].MYT4UList[0].countShare == 0)
+                {
+                    GameManager.instace.playerList[x].MYT4UList.RemoveAt(0);
+                    GameManager.instace.playerList[x].hasMYT4U = false;
+                    //GameManager.instace.playerList[x].MYT4UList[0].pricePerShare = 0;
+                }
+            }
+
+        }
+        if (SmallDealDeckController.instance.usedCards[SmallDealDeckController.instance.cardcount - 1].GRO4US == true)
+        {
+
+            if (GameManager.instace.playerList[x].hasGRO4US == true)
+            {
+                GameManager.instace.playerList[x].GRO4USList[0].pricePerShare = ((input * SmallDealDeckController.instance.usedCards[SmallDealDeckController.instance.cardcount - 1].value)
+                    + (GameManager.instace.playerList[x].GRO4USList[0].countShare * GameManager.instace.playerList[x].GRO4USList[0].pricePerShare))
+                    / (input + GameManager.instace.playerList[x].GRO4USList[0].countShare);
+                GameManager.instace.playerList[x].GRO4USList[0].countShare -= input;
+                if (GameManager.instace.playerList[x].GRO4USList[0].countShare == 0)
+                {
+                    GameManager.instace.playerList[x].GRO4USList.RemoveAt(0);
+                    GameManager.instace.playerList[x].hasGRO4US = false;
+                    //GameManager.instace.playerList[x].GRO4USList[0].pricePerShare = 0;
+                }
+            }
+
+
+        }
+        if (SmallDealDeckController.instance.usedCards[SmallDealDeckController.instance.cardcount - 1].OK4U == true)
+        {
+
+            if (GameManager.instace.playerList[x].hasOK4U == true)
+            {
+                GameManager.instace.playerList[x].OK4UList[0].pricePerShare = ((input * SmallDealDeckController.instance.usedCards[SmallDealDeckController.instance.cardcount - 1].value)
+                    + (GameManager.instace.playerList[x].OK4UList[0].countShare * GameManager.instace.playerList[x].OK4UList[0].pricePerShare))
+                    / (input + GameManager.instace.playerList[x].OK4UList[0].countShare);
+                GameManager.instace.playerList[x].OK4UList[0].countShare -= input;
+                if (GameManager.instace.playerList[x].OK4UList[0].countShare == 0)
+                {
+                    GameManager.instace.playerList[x].OK4UList.RemoveAt(0);
+                    GameManager.instace.playerList[x].hasOK4U = false;
+                    //GameManager.instace.playerList[x].OK4UList[0].pricePerShare = 0;
+                }
+            }
+
 
         }
     }
